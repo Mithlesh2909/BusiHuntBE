140e60edb0e6e77c23977fe118085fae0df185a9
Mithlesh2909 <kmb45p57qf@privaterelay.appleid.com>
2026-06-09 08:56:10 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,6 +2,12 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 
+console.log("-----",process.env.DB_USER)
+console.log("-----",process.env.DB_HOST)
+console.log("-----",process.env.DB_NAME)
+console.log("-----",process.env.DB_PASSWORD)
+console.log("-----",process.env.DB_PORT)
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
